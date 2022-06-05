4037722a1a226c354c3e0365ae084015c01c3bb3
Go.purin <purintadsorn@gmail.com>
2022-06-05 23:00:51 +0700

    Add project files.

---
+++ WebApplication1/Controllers/TestsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IronPdf;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    public class TestsController : ControllerBase
+    {
+        [HttpGet("testAPI1")]
+        public string testAPI()
+        {
+            var Renderer = new ChromePdfRenderer();
+
+            using var PDF = Renderer.RenderHtmlFileAsPdf("Html/auth_bg.html");
+            PDF.SaveAs("Authorization_for_Background Check.pdf");
+            return "Generate Compleat!";
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var sqlConnectionString = Configuration.GetConnectionString("postgres");
+            var sqlConnectionStringReadOnly = Configuration.GetConnectionString("postgresReadOnly");
+            services.AddScoped<Maincontext>();
+            //services.AddScoped<IBaseRepository, BaseRepository>();
+
+            //services.AddDbContext<Maincontext>(options =>
+            //    options.UseNpgsql(Configuration.GetConnectionString("postgres")
+            //));
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:5000")
+                                          .AllowAnyHeader()
+                                          .AllowAnyOrigin()
+                                          .AllowAnyMethod();
+                                  });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseCors(option => option.SetIsOriginAllowed(x => _ = true).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
